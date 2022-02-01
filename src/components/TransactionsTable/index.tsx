e51d6ce71da6cff0deb6a62ce514bd245e97de60
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-01-31 22:45:10 -0300

    feat(transactionstable) add base of component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12000</td>
+            <td>Desenvolimento</td>
+            <td>22/03/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1100</td>
+            <td>Casa</td>
+            <td>23/03/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
