f2373b1bd9f9e6c59a4faadc12e11b67728c8219
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-02-07 23:20:38 -0300

    feat(modal) add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
-
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('#root');
@@ -17,18 +17,16 @@ export function App() {
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
-  
+
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal
-        isOpen={isNewTransactionModalOpen}
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,28 @@
+import { Container } from './styles';
+import Modal from 'react-modal';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+
+      <Container>
+
+        <h2>Cadastrar transação</h2>
+        <input placeholder='Titulo' />
+        <input placeholder='Valor' type='number' />
+        <input placeholder='Categoria' />
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+}
