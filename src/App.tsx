12f1d32399f15c38fc32017d1053bf3f0df7d4c9
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-02-09 21:26:28 -0300

    feat(context) add base context to transactions

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,16 +20,16 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <NewTransactionModal 
-        isOpen={isNewTransactionModalOpen} 
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
